0ee30d429b71c2dbb218f9e6da35040877312917
Volodymyr <35489438+GuziyV@users.noreply.github.com>
2020-03-23 22:02:29 +0200

    Add vue project, install vue player

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="hello">
+		<audio-recorder
+		class="audio-recorder"
+		upload-url="YOUR_API_URL"
+		:attempts="3"
+		:time="2"
+		:before-recording="callback"
+		:pause-recording="callback"
+		:after-recording="callback"
+		:select-record="callback"
+		:before-upload="callback"
+		:successful-upload="callback"
+		:failed-upload="callback"/>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+    callback (data) {
+		console.log(data.blob)
+    }
+  }
+}
+</script>
+<style>
+.audio-recorder {
+	margin: 0 auto;
+}
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import AudioRecorder from 'vue-audio-recorder'
+
+Vue.config.productionTip = false;
+
+Vue.use(AudioRecorder)
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
